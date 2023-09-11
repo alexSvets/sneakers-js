dd685355a526d76017f919213eff92b426187c49
Sasha <sasha.shvets.2017@mail.ru>
2023-09-11 18:42:54 +0300

    fetch card api

--- script/index.js
+++ script/index.js
@@ -1,66 +1,3 @@
-const cards = [
-  {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 12999,
-    imageUrl: "../img/1.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Air Max 270",
-    price: 15600,
-    imageUrl: "../img/2.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 8499,
-    imageUrl: "../img/3.jpg",
-  },
-  {
-    title: "Кроссовки Puma X Aka Boku Future Rider",
-    price: 8999,
-    imageUrl: "../img/4.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Under Armour Curry 8 ",
-    price: 15199,
-    imageUrl: "../img/5.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Kyrie 7",
-    price: 11299,
-    imageUrl: "../img/6.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Jordan Air Jordan 11",
-    price: 10799,
-    imageUrl: "../img/7.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike LeBron XVIII",
-    price: 16499,
-    imageUrl: "../img/8.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Lebron XVIII Low",
-    price: 13999,
-    imageUrl: "../img/9.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 8499,
-    imageUrl: "../img/4.jpg",
-  },
-  {
-    title: "Мужские Кроссовки Nike Kyrie Flytrap IV",
-    price: 11299,
-    imageUrl: "../img/2.jpg",
-  },
-  {
-    title: "Кроссовки Puma X Aka Boku Future Rider",
-    price: 8999,
-    imageUrl: "../img/7.jpg",
-  },
-];
-
 const template = document.getElementById("template-element").content;
 const elements = document.querySelector(".elements");
 const cart = document.querySelector(".cart");
@@ -80,13 +17,27 @@ function generateItem(item) {
   return newItem;
 }
 
+// fetch card api
+function getAllCard() {
+  return fetch("https://64fb1cdbcb9c00518f7aa909.mockapi.io/sneakers")
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.resolve(
+        `У вас случилась ошибка со статусом ${res.status}`
+      );
+    })
+    .then((data) => {
+      data.forEach((card) => {
+        addElement(card);
+      });
+    });
+}
+
 function addElement(element) {
   elements.prepend(generateItem(element));
 }
-
-cards.forEach((card) => {
-  addElement(card);
-});
 
 //  open/close cart
 function closeCartOverlay(event) {
@@ -110,6 +61,8 @@ function openCart() {
   cart.classList.add("cart_is-opened");
   document.addEventListener("keydown", closeCartEsc);
 }
+
+getAllCard();
 
 cartButton.addEventListener("click", openCart);
 cartButtonBack.addEventListener("click", closeCart);
